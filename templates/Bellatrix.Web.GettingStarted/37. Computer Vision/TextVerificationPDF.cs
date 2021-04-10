169637b8247acb0ee371a65b4833701fae6d4ca8
Anton Angelov <aangelov@automatetheplanet.com>
2021-04-10 13:11:29 +0300

    add new computer vision engine

---
+++ templates/Bellatrix.Web.GettingStarted/37. Computer Vision/TextVerificationPDF.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bellatrix.Web.GettingStarted
+{
+    [TestClass]
+    [Browser(BrowserType.Chrome, Lifecycle.RestartEveryTime)]
+    public class TextVerificationPDF : MSTest.WebTest
+    {
+        [TestMethod]
+        [TestCategory(Categories.CI)]
+        public void MakeTextExtractionFromPDF()
+        {
+            App.NavigationService.Navigate("http://demos.bellatrix.solutions/?add-to-cart=26");
+         }
+    }
+}
