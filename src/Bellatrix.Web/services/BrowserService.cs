d6d06105b2e8b01ac84444aa070b57b11669050e
Anton Angelov <aangelov@automatetheplanet.com>
2022-02-11 17:27:26 +0200

    updating wait for ajax

--- src/Bellatrix.Web/services/BrowserService.cs
+++ src/Bellatrix.Web/services/BrowserService.cs
@@ -284,7 +284,7 @@ namespace Bellatrix.Web
             Bellatrix.Utilities.Wait.ForConditionUntilTimeout(
                 () =>
                 {
-                    numberOfAjaxConnections = InvokeScript("return window.openHTTPs ? window.openHTTPs.length : null");
+                    numberOfAjaxConnections = InvokeScript("return !isNaN(window.openHTTPs) ? window.openHTTPs : null");
 
                     if (int.TryParse(numberOfAjaxConnections, out int ajaxConnections))
                     {
@@ -309,7 +309,7 @@ namespace Bellatrix.Web
 
         private void MonkeyPatchXMLHttpRequest()
         {
-            var numberOfAjaxConnections = InvokeScript("return window.openHTTPs ? window.openHTTPs.length : null");
+            var numberOfAjaxConnections = InvokeScript("return !isNaN(window.openHTTPs) ? window.openHTTPs : null");
 
             if (int.TryParse(numberOfAjaxConnections, out int ajaxConnections))
             {
