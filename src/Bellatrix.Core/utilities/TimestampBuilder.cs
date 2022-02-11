15350216aef866b5c17fbe0d5f0cf472768d3361
Anton Angelov <aangelov@automatetheplanet.com>
2022-02-11 17:34:43 +0200

    add new utilities

--- src/Bellatrix.Core/utilities/TimestampBuilder.cs
+++ src/Bellatrix.Core/utilities/TimestampBuilder.cs
@@ -12,6 +12,7 @@
 // <author>Anton Angelov</author>
 // <site>https://bellatrix.solutions/</site>
 using System;
+using System.Linq;
 
 namespace Bellatrix.Utilities
 {
@@ -39,6 +40,13 @@ namespace Bellatrix.Utilities
         {
             var newTimestamp = DateTime.Now.ToString("MMMMddyyyyhhmmss");
             return newTimestamp;
+        }
+
+        public static string GenerateRandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
