e151751d1c7bd896d375c9814c1090ed4b545a88
Anton Angelov <aangelov@automatetheplanet.com>
2022-08-09 16:04:40 +0300

    update license header

--- src/Bellatrix.Web/plugins/analyzation/Contracts/IExceptionAnalyser.cs
+++ src/Bellatrix.Web/plugins/analyzation/Contracts/IExceptionAnalyser.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright file="IExceptionAnalyser.cs" company="Automate The Planet Ltd.">
-// Copyright 2021 Automate The Planet Ltd.
+// Copyright 2022 Automate The Planet Ltd.
 // Licensed under the Royalty-free End-user License Agreement, Version 1.0 (the "License");
 // You may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://bellatrix.solutions/licensing-royalty-free/
