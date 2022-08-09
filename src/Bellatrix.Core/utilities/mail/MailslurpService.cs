3148d4ab203eb879617c5774d9b78f8761d133c9
Anton Angelov <aangelov@automatetheplanet.com>
2022-08-09 19:17:43 +0300

    add MailslurpService

--- src/Bellatrix.Core/utilities/mail/MailslurpService.cs
+++ src/Bellatrix.Core/utilities/mail/MailslurpService.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Bellatrix.Infrastructure.SystemFacades;
+using mailslurp.Api;
+using mailslurp.Client;
+using mailslurp.Model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bellatrix.Utilities;
 
 public class MailslurpService
 {
+    private const long TIMEOUT = 30000L;
+    private InboxControllerApi inboxControllerApi;
+
+    public MailslurpService(string apiKey)
+    {
+        var config = new Configuration();
+        config.ApiKey.Add("x-api-key", apiKey);
+        inboxControllerApi = new InboxControllerApi(config);
+    }
+
+    public InboxDto CreateInbox(string name)
+    {
+        var inbox = inboxControllerApi.CreateInbox(name: name);
+
+        return inbox;
+    }
+
+    public mailslurp.Model.Email WaitForLatestEmail(InboxDto inbox, DateTime? since)
+    {
+        var waitForControllerApi = new WaitForControllerApi();
+        mailslurp.Model.Email receivedEmail = waitForControllerApi.WaitForLatestEmail(inbox.Id, TIMEOUT, false, null, since, null, 10000L);
+
+        return receivedEmail;
+    }
+
+    public void SendEmail(InboxDto inbox, string toEmail, string subject, string templateName)
+    {
+        var emailBody = new EmbeddedResourcesService().FromFile(templateName);
+        // send HTML body email
+        var sendEmailOptions = new SendEmailOptions();
+        sendEmailOptions.To = new List<string> { toEmail };
+        sendEmailOptions.Subject = subject;
+        sendEmailOptions.Body = emailBody;
+
+        inboxControllerApi.SendEmail(inbox.Id, sendEmailOptions);
+    }
 }
