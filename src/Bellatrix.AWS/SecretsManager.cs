a145892e3d650d055b5cf11e4efdccf5f361dbca
Anton Angelov <aangelov@automatetheplanet.com>
2022-08-11 17:11:08 +0300

    add new SecretsManager AWS integration

---
+++ src/Bellatrix.AWS/SecretsManager.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="SecretsManager.cs" company="Automate The Planet Ltd.">
+// Copyright 2022 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Anton Angelov</author>
+// <site>https://bellatrix.solutions/</site>
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace Bellatrix.AWS;
+
+public static class SecretsManager
+{
+    public static string GetSecret(RegionEndpoint region, string name)
+    {
+        using var secretsManager = InitializeSecretsManagerClient(region);
+        var request = new GetSecretValueRequest
+        {
+            SecretId = name
+        };
+        var secret = secretsManager.GetSecretValueAsync(request).Result;
+        return secret.SecretString;
+    }
+
+    private static AmazonSecretsManagerClient InitializeSecretsManagerClient(RegionEndpoint region)
+    {
+        var config = new AmazonSecretsManagerConfig();
+        config.RegionEndpoint = region;
+        var client = new AmazonSecretsManagerClient(config);
+        return client;
+    }
+}
