329caa9b0e82e2293e7c528b40989bb7b5766f23
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-03-14 15:26:58 +0200

    Implemented angular find strategies

---
+++ src/Bellatrix.Playwright/findstrategies/Angular/ElementRepositoryExtensions.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ElementRepositoryExtensions.cs" company="Automate The Planet Ltd.">
+// Copyright 2024 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Miriam Kyoseva</author>
+// <site>https://bellatrix.solutions/</site>
+
+using Bellatrix.Playwright.Services;
+
+namespace Bellatrix.Playwright.Angular;
+
+public static class ElementRepositoryExtensions
+{
+    public static TComponent CreateByNgBinding<TComponent>(this ComponentCreateService repository, string binding, bool shouldCacheElement = false)
+        where TComponent : Component => repository.Create<TComponent, ByNgBinding>(new ByNgBinding(binding), shouldCacheElement);
+
+    public static TComponent CreateByNgModel<TComponent>(this ComponentCreateService repository, string model, bool shouldCacheElement = false)
+        where TComponent : Component => repository.Create<TComponent, ByNgModel>(new ByNgModel(model), shouldCacheElement);
+
+    public static TComponent CreateByNgRepeater<TComponent>(this ComponentCreateService repository, string repeater, bool shouldCacheElement = false)
+        where TComponent : Component => repository.Create<TComponent, ByNgRepeater>(new ByNgRepeater(repeater), shouldCacheElement);
+
+    public static TComponent CreateByNgSelectedOption<TComponent>(this ComponentCreateService repository, string selectedOption, bool shouldCacheElement = false)
+        where TComponent : Component => repository.Create<TComponent, ByNgSelectedOption>(new ByNgSelectedOption(selectedOption), shouldCacheElement);
+
+    public static ComponentsList<TComponent> CreateAllByNgBinding<TComponent>(this ComponentCreateService repository, string binding, bool shouldCacheElement = false)
+        where TComponent : Component => new ComponentsList<TComponent>(new ByNgBinding(binding), null, shouldCacheElement);
+
+    public static ComponentsList<TComponent> CreateAllByNgModel<TComponent>(this ComponentCreateService repository, string model, bool shouldCacheElement = false)
+        where TComponent : Component => new ComponentsList<TComponent>(new ByNgModel(model), null, shouldCacheElement);
+
+    public static ComponentsList<TComponent> CreateAllByNgRepeater<TComponent>(this ComponentCreateService repository, string repeater, bool shouldCacheElement = false)
+        where TComponent : Component => new ComponentsList<TComponent>(new ByNgRepeater(repeater), null, shouldCacheElement);
+
+    public static ComponentsList<TComponent> CreateAllByNgSelectedOption<TComponent>(this ComponentCreateService repository, string selectedOption, bool shouldCacheElement = false)
+        where TComponent : Component => new ComponentsList<TComponent>(new ByNgSelectedOption(selectedOption), null, shouldCacheElement);
+}
---
+++ src/Bellatrix.Playwright/findstrategies/Angular/PlaywrightJavaScriptLocator.cs
@@ -0,0 +1,94 @@
+﻿// <copyright file="JavaScriptBy.cs" company="Automate The Planet Ltd.">
+// Copyright 2024 Automate The Planet Ltd.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// <author>Miriam Kyoseva</author>
+// <site>https://bellatrix.solutions/</site>
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using Bellatrix.Playwright.Services;
+
+namespace Bellatrix.Playwright.Locators;
+
+/// <summary>
+/// Provides a mechanism by which to find elements within a document using JavaScript.
+/// </summary>
+public class PlaywrightJavaScriptLocator
+{
+    private readonly string _script;
+    private readonly object[] _args;
+
+    public PlaywrightJavaScriptLocator(string script, params object[] args)
+    {
+        _script = script;
+        _args = args;
+    }
+
+    public object[] AdditionalScriptArguments { get; set; }
+
+    public ILocator FindElement(IPage searchContext)
+    {
+        ReadOnlyCollection<ILocator> elements = FindElements(searchContext);
+        if (elements.Count == 0)
+        {
+            throw new ArgumentException($"Unable to locate element.");
+        }
+
+        return elements[0];
+    }
+
+    public ReadOnlyCollection<ILocator> FindElements(IPage context)
+    {
+        object[] scriptArgs = _args;
+        if (AdditionalScriptArguments != null && AdditionalScriptArguments.Length > 0)
+        {
+            scriptArgs = new object[_args.Length + AdditionalScriptArguments.Length];
+            _args.CopyTo(scriptArgs, 0);
+            AdditionalScriptArguments.CopyTo(scriptArgs, _args.Length);
+        }
+
+        // This might not work
+        // TODO TEST
+        return DeserializeJsonElement((JsonElement)ServicesCollection.Current.Resolve<JavaScriptService>().Execute(_script, scriptArgs));
+    }
+
+    public ILocator FindElement(ILocator searchContext)
+    {
+        ReadOnlyCollection<ILocator> elements = FindElements(searchContext);
+        if (elements.Count == 0)
+        {
+            throw new ArgumentException($"Unable to locate element.");
+        }
+
+        return elements[0];
+    }
+
+    public ReadOnlyCollection<ILocator> FindElements(ILocator context)
+    {
+        object[] scriptArgs = _args;
+        if (AdditionalScriptArguments != null && AdditionalScriptArguments.Length > 0)
+        {
+            scriptArgs = new object[_args.Length + AdditionalScriptArguments.Length];
+            _args.CopyTo(scriptArgs, 0);
+            AdditionalScriptArguments.CopyTo(scriptArgs, _args.Length);
+        }
+
+        // This might not work
+        // TODO TEST
+        return DeserializeJsonElement(ServicesCollection.Current.Resolve<JavaScriptService>().Execute(_script, context, scriptArgs));
+    }
+
+    private static ReadOnlyCollection<ILocator> DeserializeJsonElement(JsonElement? jsonElement)
+    {
+        byte[] jsonBytes = JsonSerializer.SerializeToUtf8Bytes(jsonElement);
+
+        return JsonSerializer.Deserialize<ReadOnlyCollection<ILocator>>(jsonBytes) ?? new ReadOnlyCollection<ILocator>(new List<ILocator>(0));
+    }
+}
