9492fb9d4cd85da98bc6a3365cf08eff32b3cc9f
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-03-20 17:00:38 +0200

    Moved the options to another folder in SyncPlaywright as they are used in BrowserPage as well
    + added to the MapperConfiguration maps for converting the options to page options

--- src/Bellatrix.Playwright/syncplaywright/getbyoptions/MapperService.cs
+++ src/Bellatrix.Playwright/syncplaywright/getbyoptions/MapperService.cs
@@ -27,21 +27,27 @@ internal static class MapperService
         {
             cfg.CreateMap<GetByAltTextOptions, LocatorGetByAltTextOptions>();
             cfg.CreateMap<GetByAltTextOptions, FrameLocatorGetByAltTextOptions>();
+            cfg.CreateMap<GetByAltTextOptions, PageGetByAltTextOptions>();
 
             cfg.CreateMap<GetByLabelOptions, LocatorGetByLabelOptions>();
             cfg.CreateMap<GetByLabelOptions, FrameLocatorGetByLabelOptions>();
+            cfg.CreateMap<GetByLabelOptions, PageGetByLabelOptions>();
 
             cfg.CreateMap<GetByPlaceholderOptions, LocatorGetByPlaceholderOptions>();
             cfg.CreateMap<GetByPlaceholderOptions, FrameLocatorGetByPlaceholderOptions>();
+            cfg.CreateMap<GetByPlaceholderOptions, PageGetByPlaceholderOptions>();
 
             cfg.CreateMap<GetByRoleOptions, LocatorGetByRoleOptions>();
             cfg.CreateMap<GetByRoleOptions, FrameLocatorGetByRoleOptions>();
+            cfg.CreateMap<GetByRoleOptions, PageGetByRoleOptions>();
 
             cfg.CreateMap<GetByTextOptions, LocatorGetByTextOptions>();
             cfg.CreateMap<GetByTextOptions, FrameLocatorGetByTextOptions>();
+            cfg.CreateMap<GetByTextOptions, PageGetByTextOptions>();
 
             cfg.CreateMap<GetByTitleOptions, LocatorGetByTitleOptions>();
             cfg.CreateMap<GetByTitleOptions, FrameLocatorGetByTitleOptions>();
+            cfg.CreateMap<GetByTitleOptions, PageGetByTitleOptions>();
         });
 
         Mapper = Config.CreateMapper();
