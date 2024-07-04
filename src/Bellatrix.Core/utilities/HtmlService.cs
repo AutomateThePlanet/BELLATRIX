b1b8eef41f84b727f85d49bcd8c687b308457f5f
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-07-04 11:13:04 +0300

    Update HtmlService.cs

--- src/Bellatrix.Core/utilities/HtmlService.cs
+++ src/Bellatrix.Core/utilities/HtmlService.cs
@@ -15,6 +15,7 @@
 using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.XPath;
+using DocumentFormat.OpenXml.EMMA;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,37 @@ namespace Bellatrix.Core.Utilities;
 
 public static class HtmlService
 {
-    private static IDocument ParseHtml(string html)
+    private static string ChildCombinator => " > ";
+    private static string Node => "/";
+    private static string NodeOrSelf => "//";
+    private static string RootElementTag => "bellatrix-root";
+
+    public static IDocument AddRootElementIfNeeded(IDocument doc)
+    {
+        var topLevelElements = doc.ChildNodes.OfType<IElement>().ToList();
+        bool hasMultipleTopLevelElements = topLevelElements.Count > 1;
+
+        if (hasMultipleTopLevelElements)
+        {
+            var root = doc.CreateElement(RootElementTag);
+
+            foreach (var node in doc.ChildNodes.ToList())
+            {
+                root.AppendChild(node.Clone(true));
+            }
+
+            while (doc.ChildNodes.Length > 0)
+            {
+                doc.RemoveChild(doc.ChildNodes[0]);
+            }
+
+            doc.AppendChild(root);
+        }
+
+        return doc;
+    }
+
+    public static IDocument ParseHtml(string html)
     {
         var config = Configuration.Default.WithXPath();
         var context = BrowsingContext.New(config);
@@ -33,104 +64,9 @@ public static class HtmlService
         return context.OpenAsync(req => req.Content(html)).Result;
     }
 
-    public static string FindCssLocator(string html, string xpath)
+    public static string ConvertAbsoluteXpathToCss(string xpath)
     {
-        var absoluteXpath = FindElementAbsoluteXpath(html, xpath);
-
-        return ConvertAbsoluteXpathToCss(absoluteXpath);
-    }
-
-    public static string FindRelativeCssLocator(IElement element, string xpath)
-    {
-        var absoluteXpath = FindRelativeElementAbsoluteXpath(element, xpath);
-
-        return ConvertAbsoluteXpathToCss(absoluteXpath);
-    }
-
-    public static List<string> FindCssLocators(string html, string xpath)
-    {
-        var absoluteXpaths = FindElementsAbsoluteXpath(html, xpath);
-
-        return ConvertAbsoluteXpathToCss(absoluteXpaths);
-    }
-
-    public static List<string> FindRelativeCssLocators(IElement element, string xpath)
-    {
-        var absoluteXpaths = FindRelativeElementsAbsoluteXpath(element, xpath);
-
-        return ConvertAbsoluteXpathToCss(absoluteXpaths);
-    }
-
-    public static IElement FindNodeByCss(string html, string css)
-    {
-        var doc = ParseHtml(html);
-
-
-        return doc.QuerySelector(css) ?? throw new ArgumentException($"No element was found with the css: {css}");
-    }
-
-    public static IElement FindNodeByXpath(string html, string xpath)
-    {
-        var doc = ParseHtml(html);
-
-        return doc.Body.SelectSingleNode(xpath).ParentElement ?? throw new ArgumentException($"No element was found with the xpath: {xpath}");
-    }
-
-    private static string FindElementAbsoluteXpath(string html, string xpath)
-    {
-        var doc = ParseHtml(html);
-        
-        var node = doc.Body.SelectSingleNode(xpath);
-
-        if (node == null) throw new ArgumentException($"No element was found with the xpath: {xpath}");
-
-        return GetAbsoluteXPath(node);
-    }
-
-    public static List<string> FindElementsAbsoluteXpath(string html, string xpath)
-    {
-        var doc = ParseHtml(html);
-        var nodes = doc.Body.SelectNodes(xpath).ToList();
-
-        if (!nodes.Any()) throw new ArgumentException($"No elements were found with the xpath: {xpath}");
-
-        var individualQueries = new List<string>();
-        foreach (var node in nodes)
-        {
-            individualQueries.Add(GetAbsoluteXPath(node));
-        }
-
-        return individualQueries;
-    }
-
-    private static string FindRelativeElementAbsoluteXpath(IElement baseElement, string xpath)
-    {
-        var el = baseElement.SelectSingleNode(xpath);
-
-        if (el == null) throw new ArgumentException($"No element was found with the xpath: {xpath}");
-
-        return GetAbsoluteXPath(el);
-    }
-
-    private static List<string> FindRelativeElementsAbsoluteXpath(INode baseElement, string xpath)
-    {
-        var elements = baseElement.ParentElement.SelectNodes(xpath).ToList();
-
-        if (!elements.Any()) throw new ArgumentException($"No elements were found with the xpath: {xpath}");
-
-        var queries = new List<string>();
-
-        foreach (var node in elements)
-        {
-            queries.Add(GetAbsoluteXPath(node));
-        }
-
-        return queries;
-    }
-
-    private static string ConvertAbsoluteXpathToCss(string xpath)
-    {
-        string cssSelector = xpath.Replace("/", " > ");
+        string cssSelector = xpath.Replace(Node, ChildCombinator);
 
         // Use regular expression to replace type[number] with :nth-child(number of type)
         var pattern = new Regex(@"(\w+)\[(\d+)\]");
@@ -143,14 +79,15 @@ public static class HtmlService
             builder.Append(cssSelector.Substring(lastIndex, match.Index - lastIndex));
             string type = match.Groups[1].Value;
             int number = int.Parse(match.Groups[2].Value);
-            builder.Append(":nth-child(").Append(number).Append(" of ").Append(type).Append(")");
+            builder.Append($"{type}:nth-of-type({number})");
+            //builder.Append(":nth-child(").Append(number).Append(" of ").Append(type).Append(")");
             lastIndex = match.Index + match.Length;
         }
         builder.Append(cssSelector.Substring(lastIndex));
 
         var locator = builder.ToString();
 
-        if (locator.StartsWith(" > "))
+        if (locator.StartsWith(ChildCombinator))
         {
             locator = locator.Substring(3);
         }
@@ -158,48 +95,53 @@ public static class HtmlService
         return locator;
     }
 
-    private static List<string> ConvertAbsoluteXpathToCss(List<string> queries)
+    public static string GetAbsoluteXpath(INode node)
     {
-        for (int i = 0; i < queries.Count; i++)
+        StringBuilder xpath = new StringBuilder(Node);
+        var currentNode = node;
+        while (currentNode != null)
         {
-            queries[i] = ConvertAbsoluteXpathToCss(queries[i]);
-        }
-
-        return queries;
-    }
-
-    // TODO: Maybe use IElement and TagName, instead of INode and NodeName.ToLower()
-    private static string GetAbsoluteXPath(INode node)
-    {
-        var xpath = new StringBuilder("/");
-
-        foreach (var ancestor in node.Ancestors())
-        {
-            if (ancestor.NodeName.ToLower().Equals("html") || ancestor.NodeName.ToLower().Equals("body") || ancestor.NodeName.StartsWith("#"))
+            if (currentNode.NodeName.ToLower().Equals("html") ||  currentNode.NodeName.ToLower().Equals("body") ||  currentNode.NodeName.StartsWith("#"))
             {
-                // ignore the <html> and <body>
-                continue;
+                // ignore the <html> and <body> and other invalid element tags
+                // ignore added BELLATRIX root element
+                // ignore invalid element tags
+                break;
             }
 
-            int index = 1;
+            xpath.Insert(0, IndexElement(currentNode));
 
-            var sibling = ancestor.PreviousSibling;
-
-            while (sibling != null && !sibling.NodeName.StartsWith("#"))
-            {
-                if (sibling.NodeName.ToLower().Equals(ancestor.NodeName.ToLower()))
-                {
-                    index++;
-                }
-
-                sibling = sibling.PreviousSibling;
-            }
-
-            xpath.Insert(0, $"/{ancestor.NodeName.ToLower()}[{index}]");
+            currentNode = currentNode.Parent;
         }
-
-        xpath.Append(node.NodeName.ToLower());
 
         return xpath.ToString();
+    }
+
+    private static string IndexElement(INode element)
+    {
+        int index = 1;
+
+        var previousSibling = element.PreviousSibling;
+        while (previousSibling != null)
+        {
+            if (previousSibling.NodeName.ToLower().Equals(element.NodeName.ToLower()))
+            {
+                index++;
+            }
+            previousSibling = previousSibling.PreviousSibling;
+        }
+
+        return $"/{element.NodeName.ToLower()}[{index}]";
+    }
+
+    public static string RemoveDanglingChildCombinatorsFromCss(string css)
+    {
+        // Split the CSS string by the child combinator and remove empty steps
+        var steps = css.Split(new[] { ChildCombinator }, StringSplitOptions.None)
+                       .Where(x => !string.IsNullOrWhiteSpace(x))
+                       .ToArray();
+
+        // Join the remaining steps with the child combinator operator
+        return string.Join(ChildCombinator, steps);
     }
 }
