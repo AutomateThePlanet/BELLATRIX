e4293462d3ff60a02d675153b682a1dea55e36ad
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-07-04 11:12:45 +0300

    Bellatrix.Web.Tests - Shadow DOM tests

--- tests/Bellatrix.Web.Tests/Configurations/TestPagesSettings.cs
+++ tests/Bellatrix.Web.Tests/Configurations/TestPagesSettings.cs
@@ -15,68 +15,38 @@ namespace Bellatrix.Web.Tests;
 
 public class TestPagesSettings
 {
-    public string AnchorWebPage { get; set; }
     public string AnchorLocalPage { get; set; }
-    public string ButtonWebPage { get; set; }
     public string ButtonLocalPage { get; set; }
-    public string CheckBoxWebPage { get; set; }
     public string CheckBoxLocalPage { get; set; }
-    public string ColorWebPage { get; set; }
     public string ColorLocalPage { get; set; }
-    public string DateWebPage { get; set; }
     public string DateLocalPage { get; set; }
-    public string DateTimeLocalWebPage { get; set; }
     public string DateTimeLocalLocalPage { get; set; }
-    public string DivWebPage { get; set; }
     public string GridLocalPage { get; set; }
     public string DivLocalPage { get; set; }
-    public string ElementWebPage { get; set; }
     public string ElementLocalPage { get; set; }
-    public string EmailWebPage { get; set; }
     public string EmailLocalPage { get; set; }
-    public string HeadingWebPage { get; set; }
     public string HeadingLocalPage { get; set; }
-    public string ImageWebPage { get; set; }
     public string ImageLocalPage { get; set; }
-    public string LabelWebPage { get; set; }
     public string LabelLocalPage { get; set; }
-    public string MonthWebPage { get; set; }
     public string MonthLocalPage { get; set; }
-    public string NumberWebPage { get; set; }
     public string NumberLocalPage { get; set; }
-    public string OptionWebPage { get; set; }
     public string OptionLocalPage { get; set; }
-    public string OutputWebPage { get; set; }
     public string OutputLocalPage { get; set; }
-    public string PasswordWebPage { get; set; }
     public string PasswordLocalPage { get; set; }
-    public string PhoneWebPage { get; set; }
     public string PhoneLocalPage { get; set; }
-    public string ProgressWebPage { get; set; }
     public string ProgressLocalPage { get; set; }
-    public string RadioWebPage { get; set; }
     public string RadioLocalPage { get; set; }
-    public string RangeWebPage { get; set; }
     public string RangeLocalPage { get; set; }
-    public string ResetWebPage { get; set; }
     public string ResetLocalPage { get; set; }
-    public string SearchWebPage { get; set; }
     public string SearchLocalPage { get; set; }
-    public string SelectWebPage { get; set; }
     public string SelectLocalPage { get; set; }
-    public string SpanWebPage { get; set; }
     public string SpanLocalPage { get; set; }
-    public string TextAreaWebPage { get; set; }
     public string TextAreaLocalPage { get; set; }
-    public string TextFieldWebPage { get; set; }
     public string TextFieldLocalPage { get; set; }
-    public string TimeWebPage { get; set; }
     public string TimeLocalPage { get; set; }
-    public string UrlWebPage { get; set; }
     public string UrlLocalPage { get; set; }
-    public string WeekWebPage { get; set; }
     public string WeekLocalPage { get; set; }
-    public string TableWebPage { get; set; }
     public string TableLocalPage { get; set; }
     public string LayoutPricingPage { get; set; }
+    public string ShadowDOMLocalPage { get; set; }
 }
---
+++ tests/Bellatrix.Web.Tests/Controls/Shadow DOM/ShadowDomTests.cs
@@ -0,0 +1,98 @@
+﻿using Bellatrix.Web.Components;
+using Bellatrix.Web.Tests.Controls.Table;
+using Microsoft.TeamFoundation.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bellatrix.Web.Tests.Controls.ShadowDom;
+
+[TestClass]
+[Browser(BrowserType.Chrome, Lifecycle.ReuseIfStarted)]
+public class ShadowDomTests : MSTest.WebTest
+{
+    public override void TestInit()
+    {
+        base.TestInit();
+
+        var url = ConfigurationService.GetSection<TestPagesSettings>().ShadowDOMLocalPage;
+        App.Navigation.NavigateToLocalPage(url);
+    }
+
+    [TestMethod]
+    public void CreatingShadowRootComponent()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("basicShadowHost");
+
+        Assert.IsFalse(shadowRoot.InnerHtml.IsNullOrEmpty());
+    }
+
+    [TestMethod]
+    public void FindingElementWithCss_Basic()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("basicShadowHost");
+        var select = shadowRoot.CreateByCss<Select>("[name='select']");
+
+        select.SelectByText("Is");
+
+        Assert.AreEqual("Is", select.GetSelected().InnerText);
+    }
+
+    [TestMethod]
+    public void FindingElementWithXpath_Basic()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("basicShadowHost");
+        var select = shadowRoot.CreateByXpath<Select>(".//select[@name='select']");
+
+        select.SelectByText("Is");
+
+        Assert.AreEqual("Is", select.GetSelected().InnerText);
+    }
+
+    [TestMethod]
+    public void DirectlyFindingElementInNestedShadowRoot_WithXpath()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("complexShadowHost");
+
+        var firstEditAnchor = shadowRoot.CreateByXpath<Anchor>(".//a[@href='#edit']");
+        Assert.AreEqual("edit", firstEditAnchor.InnerText);
+    }
+
+    [TestMethod]
+    public void DirectlyFindingElementInNestedShadowRoot_WithCss()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("complexShadowHost");
+
+        var firstEditAnchor = shadowRoot.CreateByCss<Anchor>("[href='#edit']");
+        Assert.AreEqual("edit", firstEditAnchor.InnerText);
+    }
+
+    [TestMethod]
+    public void FindingElementByAnotherElementInNestedShadowRoot_WithXpath()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("complexShadowHost");
+
+        var table = shadowRoot.CreateByXpath<Grid>(".//table[@id='shadowTable']")
+            .SetModelColumns<TableData>()
+            .SetColumn("Action");
+
+        var row = table.GetFirstOrDefaultRow<GridCell>(cell => cell.InnerText.Equals("jsmith@gmail.com"));
+
+        var edit = table.GetColumn("Action")[row.Index].CreateByXpath<Anchor>(".//a[@href='#edit']");
+
+        Assert.AreEqual("edit", edit.InnerText);
+    }
+
+    [TestMethod]
+    public void FindingElementByAnotherElementInNestedShadowRoot_WithCss()
+    {
+        var shadowRoot = App.Components.CreateById<ShadowRoot>("complexShadowHost");
+
+        var table = shadowRoot.CreateByCss<Grid>("[id='shadowTable']")
+            .SetModelColumns<TableData>()
+            .SetColumn("Action");
+
+        var row = table.GetFirstOrDefaultRow<GridCell>(cell => cell.InnerText.Equals("jsmith@gmail.com"));
+
+        var edit = table.GetColumn("Action")[row.Index].CreateByCss<Anchor>("[href='#edit']");
+        Assert.AreEqual("edit", edit.InnerText);
+    }
+}
