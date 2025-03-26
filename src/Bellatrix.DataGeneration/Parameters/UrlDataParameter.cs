b99a93efae54a68398b0af0fd4f179f29c45b78c
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 17:30:19 +0200

    add new url parameter, strategy, config

---
+++ src/Bellatrix.DataGeneration/Parameters/UrlDataParameter.cs
@@ -0,0 +1,23 @@
+﻿using Bellatrix.DataGeneration.TestValueProviders;
+
+namespace Bellatrix.DataGeneration.Parameters;
+
+public class UrlDataParameter : DataParameter<UrlDataProviderStrategy>
+{
+    public UrlDataParameter(
+        bool isManualMode = false,
+        int? minBoundary = null,
+        int? maxBoundary = null,
+        bool? includeBoundaryValues = null,
+        bool? allowValidEquivalenceClasses = null,
+        bool? allowInvalidEquivalenceClasses = null,
+        params TestValue[] customValues)
+        : base(new UrlDataProviderStrategy(minBoundary, maxBoundary),
+              isManualMode,
+              includeBoundaryValues,
+              allowValidEquivalenceClasses,
+              allowInvalidEquivalenceClasses,
+              customValues)
+    {
+    }
+}
---
+++ src/Bellatrix.DataGeneration/TestValueProviders/UrlDataProviderStrategy.cs
@@ -0,0 +1,36 @@
+﻿using Bellatrix.DataGeneration.Parameters;
+
+namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class UrlDataProviderStrategy : DataProviderStrategy<int>
+{
+    public UrlDataProviderStrategy(int? minBoundary = null, int? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "URL";
+
+    protected override Type GetExpectedType() => typeof(string);
+
+    protected override TestValue CreateBoundaryTestValue(int boundaryInput, TestValueCategory category)
+    {
+        // Generate "https://www.a...a.com" of appropriate length
+        const string prefix = "https://www.";
+        const string suffix = ".com";
+        int coreLength = Math.Max(1, boundaryInput - prefix.Length - suffix.Length);
+
+        string domain = new string('a', coreLength);
+        string url = prefix + domain + suffix;
+
+        return new TestValue(url, typeof(string), category);
+    }
+
+    protected override int OffsetValue(int value, BoundaryOffsetDirection direction)
+    {
+        bool parsed = int.TryParse(PrecisionStep, out int step);
+        int offset = parsed ? step : 1;
+
+        return direction == BoundaryOffsetDirection.Before ? value - offset : value + offset;
+    }
+}
