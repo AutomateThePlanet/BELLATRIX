b776a4590bd915fe52ce92ea7071d8b4d47f22f6
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 14:48:47 +0200

    add password parameter and provider, config values

---
+++ src/Bellatrix.DataGeneration/TestValueProviders/PasswordDataProviderStrategy.cs
@@ -0,0 +1,21 @@
+﻿namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class PasswordDataProviderStrategy : DataProviderStrategy
+{
+    public PasswordDataProviderStrategy(int? minBoundary = null, int? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "Password";
+
+    protected override string GenerateValue(int length)
+    {
+        // Ensure at least one uppercase, one lowercase, one digit, one special
+        string strongBase = "Aa1@";
+        if (length <= strongBase.Length)
+            return strongBase.Substring(0, length);
+
+        return strongBase + new string('x', length - strongBase.Length);
+    }
+}
