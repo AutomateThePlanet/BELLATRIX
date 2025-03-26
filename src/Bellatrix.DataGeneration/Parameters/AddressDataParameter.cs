7199fffe820e5dd1532b9e0026c91b17ecdb7d64
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 17:18:11 +0200

    add address parameter, strategy and config

---
+++ src/Bellatrix.DataGeneration/Parameters/AddressDataParameter.cs
@@ -0,0 +1,23 @@
+﻿using Bellatrix.DataGeneration.TestValueProviders;
+
+namespace Bellatrix.DataGeneration.Parameters;
+
+public class AddressDataParameter : DataParameter<AddressDataProviderStrategy>
+{
+    public AddressDataParameter(
+        bool isManualMode = false,
+        int? minBoundary = null,
+        int? maxBoundary = null,
+        bool? includeBoundaryValues = null,
+        bool? allowValidEquivalenceClasses = null,
+        bool? allowInvalidEquivalenceClasses = null,
+        params TestValue[] customValues)
+        : base(new AddressDataProviderStrategy(minBoundary, maxBoundary),
+              isManualMode,
+              includeBoundaryValues,
+              allowValidEquivalenceClasses,
+              allowInvalidEquivalenceClasses,
+              customValues)
+    {
+    }
+}
---
+++ src/Bellatrix.DataGeneration/TestValueProviders/AddressDataProviderStrategy.cs
@@ -0,0 +1,35 @@
+﻿using Bellatrix.DataGeneration.Parameters;
+
+namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class AddressDataProviderStrategy : DataProviderStrategy<int>
+{
+    public AddressDataProviderStrategy(int? minBoundary = null, int? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "Address";
+
+    protected override Type GetExpectedType() => typeof(string);
+
+    protected override TestValue CreateBoundaryTestValue(int boundaryInput, TestValueCategory category)
+    {
+        var baseAddress = "123 Main St, Springfield, ZZ 12345";
+        if (boundaryInput <= baseAddress.Length)
+        {
+            return new TestValue(baseAddress.Substring(0, boundaryInput), typeof(string), category);
+        }
+
+        string extended = baseAddress + " " + new string('A', boundaryInput - baseAddress.Length);
+        return new TestValue(extended, typeof(string), category);
+    }
+
+    protected override int OffsetValue(int value, BoundaryOffsetDirection direction)
+    {
+        bool parsed = int.TryParse(PrecisionStep, out int step);
+        int offset = parsed ? step : 1;
+
+        return direction == BoundaryOffsetDirection.Before ? value - offset : value + offset;
+    }
+}
