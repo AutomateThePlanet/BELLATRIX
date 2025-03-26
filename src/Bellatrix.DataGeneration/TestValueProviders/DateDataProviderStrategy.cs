f6445085d19deb0930fc0d80f52f2549c3aba12a
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 16:54:05 +0200

    add new date parameter and strategy

---
+++ src/Bellatrix.DataGeneration/TestValueProviders/DateDataProviderStrategy.cs
@@ -0,0 +1,27 @@
+﻿using Bellatrix.DataGeneration.Parameters;
+using System.Globalization;
+
+namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class DateDataProviderStrategy : DataProviderStrategy<DateTime>
+{
+    public DateDataProviderStrategy(DateTime? minBoundary = null, DateTime? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "Date";
+
+    protected override Type GetExpectedType() => typeof(DateTime);
+
+    protected override TestValue CreateBoundaryTestValue(DateTime boundaryInput, TestValueCategory category)
+    {
+        return new TestValue(boundaryInput.Date, typeof(DateTime), category);
+    }
+
+    protected override DateTime OffsetValue(DateTime value, BoundaryOffsetDirection direction)
+    {
+        int daysOffset = int.TryParse(PrecisionStep, out int step) ? step : 1;
+        return direction == BoundaryOffsetDirection.Before ? value.AddDays(-daysOffset) : value.AddDays(daysOffset);
+    }
+}
