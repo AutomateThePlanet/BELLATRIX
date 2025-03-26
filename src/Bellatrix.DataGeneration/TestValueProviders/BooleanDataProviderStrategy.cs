7d0b5a24470978e5d0304fbb11138e0e5e4184a1
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 14:50:10 +0200

    add boolean parameter and provider, config values

---
+++ src/Bellatrix.DataGeneration/TestValueProviders/BooleanDataProviderStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Bellatrix.DataGeneration.Parameters;
+
+namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class BooleanDataProviderStrategy : DataProviderStrategy
+{
+    public BooleanDataProviderStrategy(int? minBoundary = null, int? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "Boolean";
+
+    protected override string GenerateValue(int length)
+    {
+        // Alternate based on length to generate a realistic boolean sample
+        return length % 2 == 0 ? "true" : "false";
+    }
+}
