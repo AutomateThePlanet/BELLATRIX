cc219e33a1715c0c408c78f6b49f953b6f69e399
Anton Stanislavov Angelov <aangelov@automatetheplanet.com>
2025-03-26 16:39:11 +0200

    add percentage data parameter, provider, config

---
+++ src/Bellatrix.DataGeneration/Parameters/PercentageDataParameter.cs
@@ -0,0 +1,23 @@
+﻿using Bellatrix.DataGeneration.TestValueProviders;
+
+namespace Bellatrix.DataGeneration.Parameters;
+
+public class PercentageDataParameter : DataParameter<PercentageDataProviderStrategy>
+{
+    public PercentageDataParameter(
+        bool isManualMode = false,
+        decimal? minBoundary = null,
+        decimal? maxBoundary = null,
+        bool? includeBoundaryValues = null,
+        bool? allowValidEquivalenceClasses = null,
+        bool? allowInvalidEquivalenceClasses = null,
+        params TestValue[] customValues)
+        : base(new PercentageDataProviderStrategy(minBoundary, maxBoundary),
+              isManualMode,
+              includeBoundaryValues,
+              allowValidEquivalenceClasses,
+              allowInvalidEquivalenceClasses,
+              customValues)
+    {
+    }
+}
---
+++ src/Bellatrix.DataGeneration/TestValueProviders/PercentageDataProviderStrategy.cs
@@ -0,0 +1,29 @@
+﻿using Bellatrix.DataGeneration.Parameters;
+using System.Globalization;
+
+namespace Bellatrix.DataGeneration.TestValueProviders;
+
+public class PercentageDataProviderStrategy : DataProviderStrategy<decimal>
+{
+    public PercentageDataProviderStrategy(decimal? minBoundary = null, decimal? maxBoundary = null)
+        : base(minBoundary, maxBoundary)
+    {
+    }
+
+    protected override string GetInputTypeName() => "Percentage";
+
+    protected override Type GetExpectedType() => typeof(decimal);
+
+    protected override TestValue CreateBoundaryTestValue(decimal boundaryInput, TestValueCategory category)
+    {
+        return new TestValue(boundaryInput, typeof(decimal), category);
+    }
+
+    protected override decimal OffsetValue(decimal value, BoundaryOffsetDirection direction)
+    {
+        bool parsed = decimal.TryParse(PrecisionStep, NumberStyles.Float, CultureInfo.InvariantCulture, out decimal step);
+        decimal offset = parsed ? step : 0.01m;
+
+        return direction == BoundaryOffsetDirection.Before ? value - offset : value + offset;
+    }
+}
